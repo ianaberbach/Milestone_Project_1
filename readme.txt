4e9b2e0bc5b0434d546e42962eed98ca4582996d
Ian <naberbach@gmail.com>
2022-07-30 16:29:27 -0400

    Phil 1.0

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Simple sidescroller where Phil escapes after being bad to the bone.
+Click Start Button to Start Game.
+Features to be added:  Restart button, highscore, different obstacle model, explosion animation on death, and tweaking hit detection to be slightly more accurate.
--- script.js
+++ script.js
@@ -1,32 +1,35 @@
   var phil = document.getElementById("phil");
   var obstacle = document.getElementById("obstacle");
-//   let score = parseInt(document.querySelector(".score h2").innerHTML);
+  let score = parseInt(document.querySelector(".score h2").innerHTML);
   var vid = document.getElementById("my_video");
-  var mutebtn = document.getElementById("mutebtn");
-  let counter = 0;
+  var startBtn = document.getElementById("stBtn");
+  var audio = document.getElementById("audio");
+  var explosion = document.getElementById("audio2");
+  var splode = document.getElementById("explosion");
 
   //Adds animate class to Phil for 500ms before removing it
   //Adds 1 to the score for every jump
   function jump(){
-   if (phil.classList.contains("animate")){return}
-    phil.classList.add("animate");
-    setTimeout(() => {
-     phil.classList.remove("animate")
-    }, 500);
-//      setTimeout(function(){
-//     score+=1;
-//     document.querySelector(".score h2").innerHTML = score;
-//  }, 500);
-}
-
-
-function timer(score){
-    score++
-    console.log(score);
-    return score;
+    if (phil.classList.contains("animate")){return}
+     phil.classList.add("animate");
+     setTimeout(() => {
+      phil.classList.remove("animate")
+     }, 500);
  }
-// document.querySelector(".score h2").innerHTML = counter;
-setInterval (timer(counter), 500);
+
+ function start(){
+    if (obstacle.classList.contains("animate2")){return}
+    obstacle.classList.add("animate2");
+    function scoreCount(){
+        score += 1     
+        document.querySelector(".score h2").innerHTML = score;
+        console.log(score);
+    }
+    stop1 = setInterval(scoreCount, 1000);
+    startBtn.style.display = "none";
+    audio.play();
+    audio.loop = true;
+ }
 
 //  checks to see if Phil and the obstacle are touching
  var hitScan = setInterval(function(){
@@ -36,20 +39,15 @@ setInterval (timer(counter), 500);
          obstacle.style.animation = "none";
          obstacle.style.display = "none";
          alert("Phil was too bad for his own bones.");
+         clearInterval(stop1);
+         audio.pause();
+         audio.loop = false;
+         audio2.play();
      }
  }, 10);
+
 
 //  calls the jump function on mouseclick
   document.addEventListener("click", jump);
 
-//   mutebtn.addEventListener("canplay", vidmute);
-
-//   function vidmute(){
-//     if(vid.muted){
-//         vid.muted = false;
-//         mutebtn.innerHTML = "Muted";
-//     } else {
-//         vid.muted = true;
-//         mutebtn.innerHTML = "Unmuted";
-//     }
-//   }
+  startBtn.addEventListener("click", start)
